82181408c7d44dcb44e6888435c4f980b5d870af
K-Pecka <93806948+K-Pecka@users.noreply.github.com>
2023-06-24 12:16:10 +0200

    Add files via upload
    # Conflicts:
    #	jaz-25668-nbpService/.gitignore
    #	jaz-25668-nbpService/.mvn/wrapper/maven-wrapper.properties
    #	jaz-25668-nbpService/HELP.md
    #	jaz-25668-nbpService/mvnw
    #	jaz-25668-nbpService/mvnw.cmd
    #	jaz-25668-nbpService/pom.xml
    #	jaz-25668-nbpService/src/main/java/Nbp.java
    #	jaz-25668-nbpService/src/main/java/pl/pjwstk/jaz25668nbpService/Configuration/restTemplateConfig.java
    #	jaz-25668-nbpService/src/main/java/pl/pjwstk/jaz25668nbpService/Jaz25668NbpServiceApplication.java
    #	jaz-25668-nbpService/src/main/java/pl/pjwstk/jaz25668nbpService/Service/NbpService.java
    #	jaz-25668-nbpService/src/main/java/pl/pjwstk/jaz25668nbpService/controller/NbpController.java
    #	jaz-25668-nbpService/src/main/java/pl/pjwstk/jaz25668nbpService/model/Nbp.java
    #	jaz-25668-nbpService/src/main/java/pl/pjwstk/jaz25668nbpService/model/Rates.java
    #	jaz-25668-nbpService/src/main/resources/application.properties
    #	jaz-25668-nbpService/src/test/java/pl/pjwstk/jaz25668nbpService/Jaz25668NbpServiceApplicationTests.java

---
+++ jaz-25668-nbp/src/test/java/pl/pjwstk/jaz25668nbp/Jaz25668NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.jaz25668nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz25668NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
